34035c0e9347bfc982d5b4a463227d8fb6afd283
Abraham Yusuf <abrahamyusuf.eth@gmail.com>
2026-01-24 14:04:01 +0700

    update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       label: "Terminal",
       active: true,
     }, 
-    {/*
+    {
       icon: <span className="material-symbols-outlined text-[16px] text-black">toll</span>,
       label: "Toll_Booth",
       active: false,
@@ -60,7 +60,7 @@ export default function Home() {
       icon: <span className="material-symbols-outlined text-[16px] text-black">wallet</span>,
       label: "Wallet_Connect",
       active: false,
-    */}
+    }
   ];
 
   return (
