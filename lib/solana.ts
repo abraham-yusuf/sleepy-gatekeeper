f1c9824e593e40a19971590a3464d9e804de0d60
Abraham Yusuf <abrahamyusuf.eth@gmail.com>
2026-04-06 13:32:26 +0700

    Add Solana helper (wallet + airdrop)

---
+++ lib/solana.ts
@@ -0,0 +1,16 @@
+import { Keypair, Connection, clusterApiUrl, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+export function createWallet() {
+  const keypair = Keypair.generate();
+  return keypair;
+}
+
+export async function airdropSOL(publicKey: PublicKey) {
+  const sig = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
+  await connection.confirmTransaction(sig);
+  return sig;
+}
+
+export { connection };
